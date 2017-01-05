d77c6e80c3d4b068c5bbfd74e7aee2765f2eb05f
morettisf <morettisf@gmail.com>
2017-01-04 20:49:55 -0800

    setup of routes and first test

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
+console.log('server is running')
+
 const express = require('express')
+const bodyParser = require('body-parser')
+const routes = require('./routes/routes')
 const app = express()
+
+
 app.use(express.static('public'))
+app.use(bodyParser.json())
+
+routes(app)
+
 app.listen(process.env.PORT || 8080)
+
+module.exports = app
