b405a15b79f7bc90ceacd555bcf3335b676d5c94
morettisf <morettisf@gmail.com>
2017-01-08 21:10:04 -0800

    recipe steps

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,12 @@
 $(document).ready(function() {
   $('#date-select').datepicker()
 })
+
+var ol = document.createElement('ol')
+var stepHTML = "<li><div class='step-box'><div class='step-notes'><textarea placeholder='Write step here'></textarea></div><div class='complete'><input type='checkbox' name='step-complete'><input type='text' class='time' name='time' value='09:00 AM'></div><div class='complete-notes'><textarea placeholder='Write notes here'></textarea></div></div></li>"
+
+document.querySelector('#add-step').addEventListener('click', function() {
+  var recipeList = document.querySelector('#recipe-list')
+
+  recipeList.appendChild(ol).innerHTML += stepHTML
+})
