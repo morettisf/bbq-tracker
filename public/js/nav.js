bac3ba8b8081a95cca0f8603ce8418073cebc906
morettisf <morettisf@gmail.com>
2017-02-10 15:12:16 -0800

    mobile fix 3

--- public/js/nav.js
+++ public/js/nav.js
@@ -5,11 +5,47 @@ var hamburger = document.querySelector('#hamburger')
 if (hamburger) {
   hamburger.addEventListener('click', function() {
     var mobileOut = document.querySelector('#mobile-out')
-    mobileOut.classList.add('animateNav')
-    mobileOut.classList.toggle('nav-open')
 
-    setTimeout(function() {
-      mobileOut.classList.remove('animateNav')
-    }, 1500)
+    if (mobileOut.classList.contains('visible')) {
+
+      setTimeout(function() {
+        mobileOut.classList.add('animateNav')
+        mobileOut.classList.toggle('nav-open')
+
+        setTimeout(function() {
+          mobileOut.classList.remove('visible')
+        }, 400)
+
+      }, 0)
+      
+    }
+
+    else {
+
+      setTimeout(function() {
+        mobileOut.classList.add('visible')
+
+        setTimeout(function() {
+          mobileOut.classList.add('animateNav')
+          mobileOut.classList.toggle('nav-open')
+
+          setTimeout(function() {
+            mobileOut.classList.remove('animateNav')
+          }, 400)
+
+        }, 100)
+
+      }, 0)
+
+
+
+      // mobileOut.classList.add('animateNav')
+      // mobileOut.classList.toggle('nav-open')
+
+      // setTimeout(function() {
+      //   mobileOut.classList.remove('animateNav')
+      // }, 1500)
+
+    }
   })
 }
