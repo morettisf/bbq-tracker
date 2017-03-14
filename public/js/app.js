c7c76c6438201c2bc376501686d244fa2698c520
morettisf <morettisf@gmail.com>
2017-03-14 16:43:43 -0700

    testing formdata2

--- public/js/app.js
+++ public/js/app.js
@@ -229,22 +229,25 @@ if (save) {
 
     var logData = Object.assign({ steps: stepInfo }, basicData)
 
-    if ( navigator.userAgent.match(/Android/i)
-     || navigator.userAgent.match(/webOS/i)
-     || navigator.userAgent.match(/iPhone/i)
-     || navigator.userAgent.match(/iPad/i)
-     || navigator.userAgent.match(/iPod/i)
-     || navigator.userAgent.match(/BlackBerry/i)
-     || navigator.userAgent.match(/Windows Phone/i)
-     ) {
-      sendLog(logData)
-    }
+    // if ( navigator.userAgent.match(/Android/i)
+    //  || navigator.userAgent.match(/webOS/i)
+    //  || navigator.userAgent.match(/iPhone/i)
+    //  || navigator.userAgent.match(/iPad/i)
+    //  || navigator.userAgent.match(/iPod/i)
+    //  || navigator.userAgent.match(/BlackBerry/i)
+    //  || navigator.userAgent.match(/Windows Phone/i)
+    //  ) {
+    //   sendLog(logData)
+    // }
 
-    else {
+    // else {
 
-      var f = new FormData()
+      var f = new FormData(form)
 
-      f.append('logData', JSON.stringify(logData))
+      let blob = new Blob('logData', JSON.stringify(logData))
+      f.append(blob)
+
+      // f.append('logData', JSON.stringify(logData))
 
       f.append('pics', document.querySelector('#file1').files[0])
       f.append('pics', document.querySelector('#file2').files[0])
@@ -257,7 +260,7 @@ if (save) {
           window.location = '/log-history?message=Log%20created'
         })
 
-    }
+    // }
 
     })
 }
@@ -273,7 +276,7 @@ function xhrPromise(f) {
     })
     xhr.addEventListener('error', reject)
 
-    xhr.send(f)
+    xhr.send(f._blob())
 
   })
 }
