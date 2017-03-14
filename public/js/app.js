449aab0255e02d614431e067ce499b7c9222be19
morettisf <morettisf@gmail.com>
2017-03-14 15:23:28 -0700

    testing formdata revert

--- public/js/app.js
+++ public/js/app.js
@@ -229,18 +229,18 @@ if (save) {
 
     var logData = Object.assign({ steps: stepInfo }, basicData)
 
-    // if ( navigator.userAgent.match(/Android/i)
-    //  || navigator.userAgent.match(/webOS/i)
-    //  || navigator.userAgent.match(/iPhone/i)
-    //  || navigator.userAgent.match(/iPad/i)
-    //  || navigator.userAgent.match(/iPod/i)
-    //  || navigator.userAgent.match(/BlackBerry/i)
-    //  || navigator.userAgent.match(/Windows Phone/i)
-    //  ) {
-    //   sendLog(logData)
-    // }
+    if ( navigator.userAgent.match(/Android/i)
+     || navigator.userAgent.match(/webOS/i)
+     || navigator.userAgent.match(/iPhone/i)
+     || navigator.userAgent.match(/iPad/i)
+     || navigator.userAgent.match(/iPod/i)
+     || navigator.userAgent.match(/BlackBerry/i)
+     || navigator.userAgent.match(/Windows Phone/i)
+     ) {
+      sendLog(logData)
+    }
 
-    // else {
+    else {
 
       var f = new FormDataPolyfill()
 
@@ -257,7 +257,7 @@ if (save) {
           window.location = '/log-history?message=Log%20created'
         })
 
-    // }
+    }
 
     })
 }
